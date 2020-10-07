dc348b3b8cfb1826f3fd59cbc83d0766b6fee23c
apap26 <apap26@yandex.ru>
2020-10-07 17:26:27 +0500

    create api

---
+++ static/scripts/views.js
@@ -0,0 +1,9 @@
+/**
+ * Открыть/закрыть каталог товаров
+ */
+
+function displayModalCategories() {
+	const modalCategories_Elem = document.querySelector('#view_categories');
+	modalCategories_Elem.classList.toggle('container_display');
+}
+
