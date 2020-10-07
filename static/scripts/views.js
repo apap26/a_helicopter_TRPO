d67d051d0eecf4d91fd6a5935cd2b3ad3d9c040e
apap26 <apap26@yandex.ru>
2020-10-07 17:26:27 +0500

    create api

--- static/scripts/views.js
+++ static/scripts/views.js
@@ -4,8 +4,6 @@
 
 function displayModalCategories() {
 	const modalCategories_Elem = document.querySelector('#view_categories');
-	const page_Elem = document.querySelector('#view_page');
-	
-	modalCategories_Elem.classList.toggle('container_modal_display');
-	page_Elem.classList.toggle('container_display');
+	modalCategories_Elem.classList.toggle('container_display');
 }
+
