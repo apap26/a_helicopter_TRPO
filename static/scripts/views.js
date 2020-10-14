9aa81715ade178ba47ef25b31bec61c3ea8a97a8
apap26 <apap26@yandex.ru>
2020-10-14 17:54:34 +0500

    add new templates and static files

--- static/scripts/views.js
+++ static/scripts/views.js
@@ -4,6 +4,8 @@
 
 function displayModalCategories() {
 	const modalCategories_Elem = document.querySelector('#view_categories');
-	modalCategories_Elem.classList.toggle('container_display');
+	const page_Elem = document.querySelector('#view_page');
+	
+	modalCategories_Elem.classList.toggle('container_modal_display');
+	page_Elem.classList.toggle('container_display');
 }
-
