4926a6d25ab8af4c6b5874cc5d2a5d9c90df0a99
yomawari <anime112@bk.ru>
2020-11-06 23:52:13 +0500

    add exAmple test

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
-
+from . import views
 # Create your tests here.asdasasd
+class operations(TestCase):
+    def test_plus(self):
+        result = views.brand(1)
+        self.assertEqual(-8, result)
